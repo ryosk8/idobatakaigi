a76e6c87ee500c592208e70a419cbd7d74c9cc8f
Ryo Kotaki <ryooptimus@gmail.com>
2021-10-28 03:00:06 +0900

    create app

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { TextField } from "@material-ui/core";
 
+import { pushMessage } from "../firebase";
+
 const MessageField = ({ name, setText, text }) => {
   const [isComposed, setIsComposed] = useState(false);
-  console.log({ text });
-
   return (
     <TextField
       fullWidth={true}
@@ -14,7 +14,7 @@ const MessageField = ({ name, setText, text }) => {
         const text =e.target.value;
         if (text==='') return;
         if (e.key === "Enter") {
-          console.log("Yes");
+          pushMessage({name: 'hihi', text})
           setText("");
           e.preventDefault();
         }
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyxmioMfCAwNhWmchjlg5z-EUtJn00pZg",
+  authDomain: "idobata-59572.firebaseapp.com",
+  databaseURL:"https://idobata-59572-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "idobata-59572",
+  storageBucket: "idobata-59572.appspot.com",
+  messagingSenderId: "930557842007",
+  appId: "1:930557842007:web:8fde3918797b39f9ff4912",
+};
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const messageRef = database.ref("messages");
+
+export const pushMessage = ({ name, text }) => {
+  messageRef.push({ name, text });
+};
