5c72dc6ff97cfc332ac19cfbe2ee613fec87b313
Ryo Kotaki <ryooptimus@gmail.com>
2021-10-27 15:04:40 +0900

    save name in japanese

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -48,7 +48,7 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled,setDisabled] = useState(true);
   const [string,setString] = useState("");
-  console.log(disabled,string);
+  const [isComposed,setIsCoposed] =useState(false);
   useEffect(() => {
     const disabled = string === ""
     setDisabled(disabled);
@@ -74,12 +74,14 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown={(e)=> {
-              console.log({key:e.key});
+              if (isComposed) return 
               if(e.key === 'Enter'){
                 setName(e.target.value);
                 e.preventDefault();
               }
             }}
+            onCompositionStart={()=> setIsCoposed(true)}
+            onCompositionEnd={()=> setIsCoposed(false)}
           />
           <Button
             type="button"
