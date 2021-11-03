4ffa3292d4bfceca54ae0396525572e3b38ceeb4
Ryo Kotaki <ryooptimus@gmail.com>
2021-11-04 00:21:54 +0900

    adjust focusing

--- src/components/MessageSubmitButton.js
+++ src/components/MessageSubmitButton.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { IconButton } from "@material-ui/core";
-import SendTwoToneIcon from '@mui/icons-material/SendTwoTone';
+import SendTwoToneIcon from "@mui/icons-material/SendTwoTone";
 import { pushMessage } from "../firebase";
-const MessageSubmitButton = ({name,setText,text}) => {
+const MessageSubmitButton = ({ inputEl, name, setText, text }) => {
   return (
-    <IconButton disabled={text === ''} onClick={() =>{
-        pushMessage({name,text});
-        setText('')
-    }}>
-        <SendTwoToneIcon/>
+    <IconButton
+      disabled={text === ""}
+      onClick={() => {
+        pushMessage({ name:"Joege", text });
+        setText("");
+        inputEl.current.focus();
+      }}
+    >
+      <SendTwoToneIcon />
     </IconButton>
   );
 };
- export default MessageSubmitButton;
+export default MessageSubmitButton;
